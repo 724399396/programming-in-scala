60fd9259fc9fe404f8034b1c6126f444e4501419
liwei <liwei72439936@gmail.com>
2015-12-18 23:17:33 +0800

    update

---
+++ code/mocha.scala
@@ -0,0 +1,12 @@
+object Mocha extends App {
+    class PreferredDrink(val preference: String)
+    implicit val pref = new PreferredDrink("mocha")
+    def enjoy(name: String)(implicit drink: PreferredDrink) {
+        println("Welcome, " + name)
+        print(". Enjoy a ")
+        print(drink.preference)
+        println("!")
+    }
+    
+    enjoy("reader")
+}
