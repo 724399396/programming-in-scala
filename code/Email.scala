d5e211935afc78234d0c80c6950a952f906aa81b
liwei <liwei72439936@gmail.com>
2015-12-21 22:54:56 +0800

    update

---
+++ code/Email.scala
@@ -0,0 +1,33 @@
+object Email {
+    // The injection method (optional)
+    def apply(user: String, domain: String) = user + "@" + domain
+
+    // The extraction method (mandatory)
+    def unapply(str: String): Option[(String,String)] = {
+        val parts = str split "@"
+        if (parts.length == 2) Some(parts(0), parts(1)) else None
+    }
+}
+
+object Twice {
+    def apply(s: String): String = s + s
+    def unapply(s: String): Option[String] = {
+        val length = s. length / 2
+        val half = s.substring(0, length)
+        if (half == s.substring(length)) Some(half) else None
+    }
+}
+
+object UpperCase {
+    def unapply(s: String): Boolean = s.toUpperCase == s
+}
+
+def userTwiceUpper(s: String) = s match {
+  case Email(Twice(x @ UpperCase()), domain) =>
+    "match: " + x + " in domain " + domain
+  case _ =>
+    "no match"
+}
+
+object Domain {
+    def apply(parts: String*)
