0d7a3173d255315df190becee0ed04cccaed95a3
liwei <liwei72439936@gmail.com>
2015-12-25 23:04:43 +0800

    update

---
+++ code/NameResolver.scala
@@ -0,0 +1,27 @@
+import scala.actors.Actor
+
+object NameResolver extends Actor {
+    import java.net.{InetAddress, UnknownHostException}
+
+    def act() {
+        react {
+          case (name: String, actor: Actor) =>
+            actor ! getIp(name)
+            act()
+          case "EXIT" =>
+            println("Name resolver exiting.")
+            // quit
+          case msg =>
+            println("Unhandled message: " + msg)
+            act()
+        }
+    }
+
+    def getIp(name: String): Option[InetAddress] = {
+        try {
+            Some(InetAddress.getByName(name))
+        } catch {
+          case _:UnknownHostException => None
+        }
+    }
+}
---
+++ code/SeriousActor.scala
@@ -0,0 +1,10 @@
+import scala.actors._
+
+object SeriousActor extends Actor {
+    def act() {
+        for (i <- 1 to 5) {
+            println("To be or not to be.")
+            Thread.sleep(1000)
+        }
+    }
+}
